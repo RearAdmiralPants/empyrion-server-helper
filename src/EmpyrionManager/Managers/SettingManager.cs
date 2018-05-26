139180a6bd8afdd088af05c7d30ffc3672257a66
Paul K <pklingman@gmail.com>
2018-05-26 19:40:12 -0400

    (dev) Implement new user-based settings provider so that the user need not edit configuration files directly unless they wish to do so.

---
+++ src/EmpyrionManager/Managers/SettingManager.cs
@@ -0,0 +1,68 @@
+﻿namespace EmpyrionManager.Managers
+{
+    using System;
+    using System.Configuration;
+    public class SettingManager : ApplicationSettingsBase
+    {
+        private const string EMPYRION_DEDICATED_SERVER_PATH = "EmpyrionDedicatedServerPath";
+        private const string EMPYRION_BACKUP_DESTINATION = "EmpyrionBackupDestination";
+        private const string EMPYRION_SAVES_LOCATION = "EmpyrionSavesLocation";
+        private const string RESTORE_RECOVERY_DESTINATION = "RestoreRecoveryDestination";
+
+        [UserScopedSetting()]
+        [DefaultSettingValue("white")]
+        public string EmpyrionDedicatedServerPath
+        {
+            get
+            {
+                return ((string)this[EMPYRION_DEDICATED_SERVER_PATH]);
+            }
+            set
+            {
+                this[EMPYRION_DEDICATED_SERVER_PATH] = value;
+            }
+        }
+
+        [UserScopedSetting()]
+        [DefaultSettingValue("C:\\steamcmd\\empyrionBackup")]
+        public string EmpyrionBackupDestination
+        {
+            get
+            {
+                return ((string)this[EMPYRION_BACKUP_DESTINATION]);
+            }
+            set
+            {
+                this[EMPYRION_BACKUP_DESTINATION] = value;
+            }
+        }
+
+        [UserScopedSetting()]
+        [DefaultSettingValue("C:\\steamcmd\\empyriondedicatedserver\\EMPSaves")]
+        public string EmpyrionServerSaveLocation
+        {
+            get
+            {
+                return ((string)this[EMPYRION_SAVES_LOCATION]);
+            }
+            set
+            {
+                this[EMPYRION_SAVES_LOCATION] = value;
+            }
+        }
+
+        [UserScopedSetting()]
+        [DefaultSettingValue("C:\\steamcmd\\empRestoreRecovery")]
+        public string EmpyrionRestoreRecoveryDirectory
+        {
+            get
+            {
+                return ((string)this[RESTORE_RECOVERY_DESTINATION]);
+            }
+            set
+            {
+                this[RESTORE_RECOVERY_DESTINATION] = value;
+            }
+        }
+    }
+}
